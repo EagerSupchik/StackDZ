2336a64b3907c56954bae02d340c2faa006f1d7b
Supchik <eagersupchik@gmail.com>
2023-12-21 12:22:39 +0300

    Добавьте файлы проекта.

---
+++ StackDZ/List_struct.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "Stack.h"
+template<class T>
+class List_struct {
+	struct Node {
+		T data;
+		Node* next;//”казатель на следующий элемент
+	};
+
+	Node* Head = nullptr;
+	Node* Tail = nullptr;
+	int count = -1;
+
+public:
+	List_struct() {
+		for (size_t i = 0; i < count; i++)
+		{
+			addElement(3);
+		}
+	}
+	//добавление элемента
+	void addElement(T data) {
+		Node* newElement = new Node;//создаем новый динамический элемент
+
+		newElement->data = data;
+		//if(cout == -1)
+		if (Head == nullptr) {
+			Tail = Head = newElement;
+		}
+		else {
+			Tail->next = newElement;
+			Tail = newElement;
+		}
+		Tail->next = nullptr;
+		count++;
+	}
+
+	void printList() {
+		Node* move = Head;
+		for (size_t i = 0; i <= count; i++)
+		{
+			cout << move->data << ", ";
+			move = move->next;
+		}
+		cout << endl;
+	}
+
+	int operator[](int index) {
+		Node* move = Head;
+		if (index < 0 || index > count || count == -1) {
+			throw exception("Out of range");
+		}
+
+		for (size_t i = 0; i < index; i++)
+		{
+			move = move->next;
+		}
+		return move->data;
+	}
+
+	void pop()
+	{
+		if (count == -1)
+		{
+			throw exception("List is empty");
+		}
+		else if (count == 0)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+		}
+		else {
+			Node* current = Head;
+			for (int i = 0; i < count - 1; ++i)
+			{
+				current = current->next;
+			}
+			delete Tail;
+			Tail = current;
+			Tail->next = nullptr;
+		}
+		count--;
+	}
+};
---
+++ StackDZ/StackDZ.cpp
@@ -0,0 +1,23 @@
+﻿#include "List_struct.h"
+
+int main()
+{
+	srand(time(NULL));
+	List_struct<int> list;
+
+	for (size_t i = 0; i < 20; i++)
+	{
+		list.addElement(rand() % 9 + 1);
+	}
+
+	list.printList();
+	try {
+		cout << list[6] << endl;
+	}
+	catch (exception& E) {
+		cout << E.what() << endl;
+	}
+
+	list.pop();
+	list.printList();
+}
